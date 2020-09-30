f3eea10018dd9d2ce11391cf8bf8876fe46465bc
Gabriel Guizardi <gabrielguicar55@gmail.com>
2020-09-30 16:54:08 -0300

    busca em profundidade

--- busca-em-profundidade.py
+++ busca-em-profundidade.py
@@ -1,30 +1,26 @@
+def dfs(graph, start, visited=None):
+  if visited is None:
+    visited = set()
+    print(start)
+  visited.add(start)
+
+  for next in graph[start]:
+    if next not in visited:
+      print(next)
+      dfs(graph, next, visited)
+  return visited
+
 graph = {
-  "a": ["b"],
-  "b": ["c"],
-  "c": ["d"],
-  "d": [],
-  "e": ["f"],
-  "f": []
+  'A': set(['B', 'J']),
+  'B': set(['A', 'D', 'I']),
+  'J': set(['A', 'I', 'C']),
+  'D': set(['B', 'C', 'I', 'E', 'H']),
+  'I': set(['B', 'J', 'D']),
+  'C': set(['J', 'D']),
+  'E': set(['D', 'F']),
+  'H': set(['D', 'G']),
+  'F': set(['E', 'G']),
+  'G': set(['H', 'F'])
 }
 
-def DFS(graph, start_node, stack = [], visited = []):
-stack.append(start_node)
-
-while stack:
-  v = stack.pop()
-  if v not in visited:
-    visited.append(v)
-    for neighbor in graph[v]:
-      stack.append(neighbor)
-      return visited
-
-def dfs_get_connected_components_util(graph):
-visited = []
-
-for node in graph:
-if node not in visited:
-DFS_algo = DFS(graph, node)
-print(DFS_algo)
-visited = DFS_algo
-
-print(dfs_get_connected_components_util(graph))
+dfs(graph, 'A')
