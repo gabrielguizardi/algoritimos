a7516bf2bd8e6d1a02a101cf678c8d4bc876253a
Gabriel Guizardi <gabrielguicar55@gmail.com>
2020-09-30 16:13:16 -0300

    busca em largura

--- busca-em-largura.py
+++ busca-em-largura.py
@@ -1,24 +1,14 @@
-import random
-
-def insere_aresta(grafo, a, b):
-  if a not in grafo:
-    grafo[a] = []
-  if b not in grafo:
-    grafo[b] = []
-  grafo[a].append(b)
-  grafo[b].append(a)
-
 def busca_em_largura(grafo, vertice_do_grafo):
   fila = [] 
 
-  largura = {}
+  visitados = {}
   l = 1 
   pai = {} 
   nivel = {} 
   aresta = {} 
 
   fila.append(vertice_do_grafo)
-  largura[vertice_do_grafo] = l
+  visitados[vertice_do_grafo] = l
   pai[vertice_do_grafo] = None
   nivel[vertice_do_grafo] = 1
 
@@ -26,15 +16,14 @@ def busca_em_largura(grafo, vertice_do_grafo):
     vertice = fila.pop(0) 
 
     for vizinho in grafo.get(vertice):
-
-      if not largura.get(vizinho): 
+      if not visitados.get(vizinho): 
         fila.append(vizinho) 
         l += 1 
-        largura[vizinho] = l
+        visitados[vizinho] = l
         pai[vizinho] = vertice
         nivel[vizinho] = nivel[vertice] + 1
 
-  return largura, pai, aresta, nivel
+  return visitados
 
 grafo = {
   'A': ['B', 'J'],
@@ -49,87 +38,6 @@ grafo = {
   'G': ['H', 'F']
 }
 
-largura, pai, aresta, nivel = busca_em_largura(grafo, 'A')
+visitados = busca_em_largura(grafo, 'A')
 
-print(list(largura.keys()))
-    for vizinho in grafo.get(vertice):
-
-      if not largura.get(vizinho): 
-        fila.append(vizinho) 
-        l += 1 
-        largura[vizinho] = l
-        pai[vizinho] = vertice
-        nivel[vizinho] = nivel[vertice] + 1
-
-  return largura, pai, aresta, nivel
-
-grafo = {
-  'A': ['B', 'J'],
-  'B': ['A', 'D', 'I'],
-  'J': ['A', 'I', 'C'],
-  'D': ['B', 'C', 'I', 'E', 'H'],
-  'I': ['B', 'J', 'D'],
-  'C': ['J', 'D'],
-  'E': ['D', 'F'],
-  'H': ['D', 'G'],
-  'F': ['E', 'G'],
-  'G': ['H', 'F']
-}
-
-largura, pai, aresta, nivel = busca_em_largura(grafo, 'A')
-
-print(list(largura.keys()))
-    for vizinho in grafo.get(vertice):
-
-      if not largura.get(vizinho): 
-        fila.append(vizinho) 
-        l += 1 
-        largura[vizinho] = l
-        pai[vizinho] = vertice
-        nivel[vizinho] = nivel[vertice] + 1
-
-  return largura, pai, aresta, nivel
-
-grafo = {
-  'A': ['B', 'J'],
-  'B': ['A', 'D', 'I'],
-  'J': ['A', 'I', 'C'],
-  'D': ['B', 'C', 'I', 'E', 'H'],
-  'I': ['B', 'J', 'D'],
-  'C': ['J', 'D'],
-  'E': ['D', 'F'],
-  'H': ['D', 'G'],
-  'F': ['E', 'G'],
-  'G': ['H', 'F']
-}
-
-largura, pai, aresta, nivel = busca_em_largura(grafo, 'A')
-
-print(list(largura.keys()))
-    for vizinho in grafo.get(vertice):
-
-      if not largura.get(vizinho): 
-        fila.append(vizinho) 
-        l += 1 
-        largura[vizinho] = l
-        pai[vizinho] = vertice
-        nivel[vizinho] = nivel[vertice] + 1
-
-  return largura, pai, aresta, nivel
-
-grafo = {
-  'A': ['B', 'J'],
-  'B': ['A', 'D', 'I'],
-  'J': ['A', 'I', 'C'],
-  'D': ['B', 'C', 'I', 'E', 'H'],
-  'I': ['B', 'J', 'D'],
-  'C': ['J', 'D'],
-  'E': ['D', 'F'],
-  'H': ['D', 'G'],
-  'F': ['E', 'G'],
-  'G': ['H', 'F']
-}
-
-largura, pai, aresta, nivel = busca_em_largura(grafo, 'A')
-
-print(list(largura.keys()))
+print(list(visitados.keys()))
